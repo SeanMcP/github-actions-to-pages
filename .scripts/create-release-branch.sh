cd82f5075cfe429b9f566d8bf7b156f140930611
Sean McPherson <git@seanmcp.com>
2020-09-09 06:46:17 -0400

    Update create-release-branch.sh
    Remove all current files

--- .scripts/create-release-branch.sh
+++ .scripts/create-release-branch.sh
@@ -4,9 +4,10 @@ branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 git checkout --orphan gh-pages
 
+git rm -rf .
+
 git commit --allow-empty -m "init"
 
 git push -u origin gh-pages
 
 git checkout $branch
-
